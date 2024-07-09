788dbec0a10c9bd7e90f6fceae37e5c9a23ef78a
Rodrigo <rod_novaes@hotmail.com>
2024-07-09 19:33:03 +0200

    Add PostsTable and AnalyticsChart component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
@@ -26,6 +28,8 @@ export default function Home() {
           icon={<MessageCircle className='text-slate-500' size={72} />}
         />
       </div>
+      <AnalyticsChart />
+      <PostsTable title='Latest Posts' limit={5} />
     </>
   );
 }
